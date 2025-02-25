ffe349132206768056d27f992068cf1545b778a9
parthpranav2 <parthpshroff@gmail.com>
2025-02-25 18:28:16 +0530

    25/2/2025,18:28

--- app/src/main/java/com/example/hackverse/UserAuthentication.kt
+++ app/src/main/java/com/example/hackverse/UserAuthentication.kt
@@ -5,7 +5,10 @@ import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -19,8 +22,11 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import java.net.Authenticator
 
 class UserAuthentication : AppCompatActivity() {
+
+    var ctr = 5
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,74 +44,91 @@ class UserAuthentication : AppCompatActivity() {
         findViewById<TextView>(R.id.lblgreet).text = "Hey, ${GlobalClass.FirstName}!"
         findViewById<TextView>(R.id.lblidentifi).text = "Not ${GlobalClass.UName}?"
 
-        var ctr = 5
-
         findViewById<TextView>(R.id.lblidentifi).setOnClickListener{
             val intent = Intent(this@UserAuthentication, LoginPage::class.java)
             startActivity(intent)
             finish()
         }
 
-        findViewById<TextView>(R.id.btnsubmit).setOnClickListener {
-            if (isInternetAvailable(this)) {
-                val pinInput = findViewById<EditText>(R.id.txtpin1).text.toString().trim()
+        findViewById<Button>(R.id.btnsubmit).setOnClickListener {
+            Authenticator()
+        }
 
-                // Check if PIN input is empty
-                if (pinInput.isEmpty()) {
-                    Toast.makeText(this, "Please enter a PIN", Toast.LENGTH_SHORT).show()
-                    return@setOnClickListener
+        val txtPin1 = findViewById<TextView>(R.id.txtpin1)
+
+        txtPin1.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (s?.length == 4) {
+                    Authenticator()  // Call your function when character count is 4
                 }
-
-                // Retrieve employee email from SharedPreferences
-                val editor = getSharedPreferences("MY_SETTING", MODE_PRIVATE)
-                val empEmail = editor.getString("empEmail", null)
-
-                if (empEmail.isNullOrEmpty()) {
-                    Toast.makeText(this, "User email not found. Please log in again.", Toast.LENGTH_SHORT).show()
-                    return@setOnClickListener
-                }
-
-                val dbRef = FirebaseDatabase.getInstance().getReference("User").child(empEmail)
-
-                // Query the "empPin" field
-                dbRef.child("empPin").addListenerForSingleValueEvent(object : ValueEventListener {
-                    override fun onDataChange(snapshot: DataSnapshot) {
-                        if (snapshot.exists()) {
-                            val empPin = snapshot.getValue(String::class.java) // Get the value of empPin
-
-                            if (empPin == pinInput) {
-                                // Navigate to Home activity
-                                val intent = Intent(this@UserAuthentication, Home::class.java)
-                                startActivity(intent)
-                                finish() // Optional: Finish current activity
-                            } else {
-                                findViewById<LinearLayout>(R.id.linelwrongpin).visibility = View.VISIBLE
-                                findViewById<EditText>(R.id.txtpin1).text.clear()
-                                findViewById<EditText>(R.id.txtpin1).background = ContextCompat.getDrawable(this@UserAuthentication, R.drawable.pinrejection)
-                                if(ctr>0){
-                                    ctr--
-                                }else{
-                                    finishAffinity()
-                                }
-                                findViewById<TextView>(R.id.txtcounterst).text="Only "+ctr+ " attempts are left."
-                            }
-                        } else {
-                            // No empPin found for the user
-                            Toast.makeText(this@UserAuthentication, "PIN not set for this user.", Toast.LENGTH_SHORT).show()
-                        }
-                    }
-
-                    override fun onCancelled(error: DatabaseError) {
-                        // Handle database error
-                        Toast.makeText(this@UserAuthentication, "Error: ${error.message}", Toast.LENGTH_SHORT).show()
-                    }
-                })
-            }else{
-                val intent = Intent(this, Offline::class.java)
-                startActivity(intent)
             }
 
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
+    }
+
+    fun Authenticator(){
+        if (isInternetAvailable(this)) {
+            val pinInput = findViewById<EditText>(R.id.txtpin1).text.toString().trim()
+
+            // Check if PIN input is empty
+            if (pinInput.isEmpty()) {
+                Toast.makeText(this, "Please enter a PIN", Toast.LENGTH_SHORT).show()
+                return
+            }
+
+            // Retrieve employee email from SharedPreferences
+            val editor = getSharedPreferences("MY_SETTING", MODE_PRIVATE)
+            val empEmail = editor.getString("empEmail", null)
+
+            if (empEmail.isNullOrEmpty()) {
+                Toast.makeText(this, "User email not found. Please log in again.", Toast.LENGTH_SHORT).show()
+                return
+            }
+
+            val dbRef = FirebaseDatabase.getInstance().getReference("User").child(empEmail)
+
+            // Query the "empPin" field
+            dbRef.child("empPin").addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()) {
+                        val empPin = snapshot.getValue(String::class.java) // Get the value of empPin
+
+                        if (empPin == pinInput) {
+                            // Navigate to Home activity
+                            findViewById<Button>(R.id.btnsubmit).text="Getting in..."
+                            val intent = Intent(this@UserAuthentication, Home::class.java)
+                            startActivity(intent)
+                            finish() // Optional: Finish current activity
+                        } else {
+                            findViewById<LinearLayout>(R.id.linelwrongpin).visibility = View.VISIBLE
+                            findViewById<EditText>(R.id.txtpin1).text.clear()
+                            findViewById<EditText>(R.id.txtpin1).background = ContextCompat.getDrawable(this@UserAuthentication, R.drawable.pinrejection)
+                            if(ctr>0){
+                                ctr--
+                            }else{
+                                finishAffinity()
+                            }
+                            findViewById<TextView>(R.id.txtcounterst).text="Only "+ctr+ " attempts are left."
+                        }
+                    } else {
+                        // No empPin found for the user
+                        Toast.makeText(this@UserAuthentication, "PIN not set for this user.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    // Handle database error
+                    Toast.makeText(this@UserAuthentication, "Error: ${error.message}", Toast.LENGTH_SHORT).show()
+                }
+            })
+        }else{
+            val intent = Intent(this, Offline::class.java)
+            startActivity(intent)
         }
+
     }
 
     fun isInternetAvailable(context: Context): Boolean {
